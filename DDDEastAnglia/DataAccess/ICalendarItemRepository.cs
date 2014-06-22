96a98cfe1189a7fa40e8bca41d49e971dd63ee6d
Alastair Smith <github@alastairsmith.me.uk>
2014-06-22 09:55:37 +0100

    Remove repository method `GetAll()`.
    This has been completely replaced with a new query object, so can be
    deleted.

--- DDDEastAnglia/DataAccess/ICalendarItemRepository.cs
+++ DDDEastAnglia/DataAccess/ICalendarItemRepository.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections.Generic;
-using DDDEastAnglia.DataAccess.SimpleData.Models;
+﻿using DDDEastAnglia.DataAccess.SimpleData.Models;
 using DDDEastAnglia.Domain.Calendar;
 
 namespace DDDEastAnglia.DataAccess
 {
     public interface ICalendarItemRepository
     {
-        IEnumerable<CalendarItem> GetAll();
         CalendarItem GetFromType(CalendarEntryType voting);
     }
 }
--- DDDEastAnglia/DataAccess/SimpleData/CalendarItemRepository.cs
+++ DDDEastAnglia/DataAccess/SimpleData/CalendarItemRepository.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using DDDEastAnglia.DataAccess.SimpleData.Models;
+﻿using DDDEastAnglia.DataAccess.SimpleData.Models;
 using DDDEastAnglia.Domain.Calendar;
 using Simple.Data;
 
@@ -8,11 +7,6 @@ namespace DDDEastAnglia.DataAccess.SimpleData
     public class CalendarItemRepository : ICalendarItemRepository
     {
         private readonly dynamic db = Database.OpenNamedConnection("DDDEastAnglia");
-
-        public IEnumerable<CalendarItem> GetAll()
-        {
-            return db.CalendarItems.All();
-        }
 
         public CalendarItem GetFromType(CalendarEntryType voting)
         {
