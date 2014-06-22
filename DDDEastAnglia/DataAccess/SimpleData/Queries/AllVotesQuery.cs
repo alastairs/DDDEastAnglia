ae29c2c4a086f333264393772d0904b0bce53c09
Alastair Smith <github@alastairsmith.me.uk>
2014-06-22 16:41:37 +0100

    Add new `AllVotesQuery` object.
    This will replace `IVoteRepository.GetAllVotes()`.

---
+++ DDDEastAnglia/DataAccess/SimpleData/Queries/AllVotesQuery.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using DDDEastAnglia.DataAccess.SimpleData.Models;
+using Simple.Data;
+
+namespace DDDEastAnglia.DataAccess.SimpleData.Queries
+{
+    public class AllVotesQuery
+    {
+        private readonly dynamic db = Database.OpenNamedConnection("DDDEastAnglia");
+
+        public IEnumerable<Vote> Execute()
+        {
+            return db.Votes.All();
+        } 
+    }
+}
