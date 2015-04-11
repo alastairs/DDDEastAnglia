b9d53a184ba47c17d9c832ccc5538a731655e346
Alastair Smith <alastair.smith@red-gate.com>
2015-04-11 22:09:52 +0100

    Test the template substitutes the abstract.
    Also extract a little System Under Test (SUT) Factory Method.

--- DDDEastAnglia.Tests/Services/Messengers/Email/Templates/SessionCreatedMailTemplateFacts.cs
+++ DDDEastAnglia.Tests/Services/Messengers/Email/Templates/SessionCreatedMailTemplateFacts.cs
@@ -1,4 +1,5 @@
-﻿using DDDEastAnglia.Models;
+﻿using DDDEastAnglia.Helpers;
+using DDDEastAnglia.Models;
 using DDDEastAnglia.Services.Messenger.Email.Templates;
 using NUnit.Framework;
 
@@ -13,12 +14,35 @@ namespace DDDEastAnglia.Tests.Services.Messengers.Email.Templates
             [Test]
             public void Substitute_The_Session_Title()
             {
-                var template = SessionCreatedMailTemplate.Create(@"Services\Messengers\Email\Templates\SessionCreated.txt", new Session { Title = "My awesome session" });
+                var template = CreateSut(title: "My awesome session");
 
                 var renderedTemplate = template.Render();
 
                 Assert.That(renderedTemplate, Is.Not.StringContaining("[SessionTitle]"));
                 Assert.That(renderedTemplate, Contains.Substring("My awesome session"));
+            }
+
+            [Test]
+            public void Substitute_The_Session_Abstract()
+            {
+                var template = CreateSut(@abstract: "Vote for me!");
+
+                var renderedTemplate = template.Render();
+
+                Assert.That(renderedTemplate, Is.Not.StringContaining("[SessionAbstract]"));
+                Assert.That(renderedTemplate, Contains.Substring("Vote for me!"));
+            }
+
+            private static IMailTemplate CreateSut(string title = "", string @abstract = "")
+            {
+                var template = SessionCreatedMailTemplate.Create(@"Services\Messengers\Email\Templates\SessionCreated.txt",
+                    new Session
+                    {
+                        Title = title,
+                        Abstract = @abstract
+                    });
+
+                return template;
             }
         }
     }
