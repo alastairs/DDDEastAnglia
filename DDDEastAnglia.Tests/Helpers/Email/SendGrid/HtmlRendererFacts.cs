a6877d150ba1e43b6862cb3f70a15a946e57bfb6
Alastair Smith <alastair.smith@red-gate.com>
2015-04-11 22:22:36 +0100

    The `HtmlRenderer` wraps the content in HTML.
    Add a test for this. It's not completely robust, but I can't see exactly
    how to make the string test better, given that the Markdown conversion
    adds a crappy newline in at the end of the content block.

--- DDDEastAnglia.Tests/Helpers/Email/SendGrid/HtmlRendererFacts.cs
+++ DDDEastAnglia.Tests/Helpers/Email/SendGrid/HtmlRendererFacts.cs
@@ -19,6 +19,18 @@ namespace DDDEastAnglia.Tests.Helpers.Email.SendGrid
                 Assert.That(renderedHtml, Contains.Substring("<p><em>italicised text</em></p>"));
             }
 
+            [Test]
+            public void Wrap_The_Content_In_An_HTML_Envelope()
+            {
+                var sut = CreateSut();
+
+                var renderedHtml = sut.Render("");
+
+                Assert.That(renderedHtml, Is.Not.StringContaining("[MessageBody]"));
+                Assert.That(renderedHtml, Is.StringStarting("<html><body>"));
+                Assert.That(renderedHtml, Is.StringEnding("</body></html>"));
+            }
+
             private static HtmlRenderer CreateSut()
             {
                 return new HtmlRenderer("<html><body>[MessageBody]</body></html>");
