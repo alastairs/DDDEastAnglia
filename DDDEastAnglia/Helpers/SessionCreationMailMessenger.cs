f4b98757095125c7fdced10f7eae253d7ce60aa4
Alastair Smith <alastair.smith@red-gate.com>
2015-04-11 19:50:03 +0100

    R#: Rename field.

--- DDDEastAnglia/Helpers/SessionCreationMailMessenger.cs
+++ DDDEastAnglia/Helpers/SessionCreationMailMessenger.cs
@@ -10,25 +10,25 @@ namespace DDDEastAnglia.Helpers
     public class SessionCreationMailMessenger : IMessenger<Session>
     {
         private readonly IPostman postman;
-        private readonly IMailTemplate plainTextTemplate;
+        private readonly IMailTemplate mailTemplate;
 
         private const string SessionAbstractToken = "[SessionAbstract]";
         private const string SessionTitleToken = "[SessionTitle]";
 
-        public SessionCreationMailMessenger(IPostman postman, IMailTemplate plainTextTemplate)
+        public SessionCreationMailMessenger(IPostman postman, IMailTemplate mailTemplate)
         {
             if (postman == null)
             {
                 throw new ArgumentNullException("postman");
             }
 
-            if (plainTextTemplate == null)
+            if (mailTemplate == null)
             {
-                throw new ArgumentNullException("plainTextTemplate");
+                throw new ArgumentNullException("mailTemplate");
             }
 
             this.postman = postman;
-            this.plainTextTemplate = plainTextTemplate;
+            this.mailTemplate = mailTemplate;
         }
 
         public void Notify(UserProfile user, Session session)
@@ -43,7 +43,7 @@ namespace DDDEastAnglia.Helpers
                 To = new MailAddress(user.EmailAddress),
                 From = new MailAddress("admin@dddeastanglia.com", "DDD East Anglia"),
                 Subject = "DDD East Anglia Session Submission: " + session.Title,
-                Body = plainTextTemplate.Render(replacementTokens)
+                Body = mailTemplate.Render(replacementTokens)
             };
 
             postman.Deliver(message);
